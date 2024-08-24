804a064d6d641e3725c90a6a05a1b5c27e9c155e
z4x <exeption.2015@gmail.com>
2024-08-24 15:43:41 -0300

    Se implementaron las siguientes modificiones: Añadir modal de galería de fotos con imagen ampliada y retorno a la galería
    - Implementada la funcionalidad de modal utilizando SweetAlert2 para mostrar una galería de fotos.
    - Añadida funcionalidad para ampliar las imágenes dentro del modal.
    - Al presionar 'Regresar a la Galería' en el modal de imagen ampliada, se retorna al modal de la galería de fotos.
    - Se agregaron estilos personalizados para la galería de fotos y el modal de imagen ampliada.

---
+++ script.js
@@ -0,0 +1,48 @@
+const galleryImages = [
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-home.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-body.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-botonConsultar.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-detallesConsulta.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-generarOrden.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-ordenGenerada.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-preguntaImpresion.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-imprimir.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-editarOrden.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-diagnosticoOrden.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-.png',
+    './assets/img/proyectos_img/fixPhones_gallery/fixPhone-.png'
+];
+
+document.getElementById('verFotos_fixPhone').addEventListener('click', () => {
+    Swal.fire({
+        title: 'Galería de Fotos',
+        html: `
+            <div class="custom-gallery">
+                ${galleryImages.map(imgSrc => `<img src="${imgSrc}" onclick="showImageModal('${imgSrc}')" />`).join('')}
+            </div>`,
+        showCloseButton: true
+    });
+});
+
+function showImageModal(imageSrc) {
+    Swal.fire({
+        imageUrl: imageSrc,
+        imageAlt: 'Imagen Ampliada',
+        showCloseButton: true,
+        confirmButtonText: 'Regresar a la Galería',
+        customClass: {
+            container: 'custom-image-modal'
+        },
+        didOpen: () => {
+            // Puedes personalizar la imagen aquí si es necesario
+        },
+        preConfirm: () => {
+            // Cuando el usuario presione "Regresar a la Galería"
+            document.getElementById('verFotos_fixPhone').click();
+        }
+    });
+}
